93ae93364a9b25d8c6f80b19c155a2a40b13a578
Md-sarfraz <mdsarfraz62070@gmail.com>
2024-10-18 23:03:28 +0530

    upadate slider

--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -26,7 +26,7 @@ export default function Slider() {
         className="mySwiper"
       >
         <SwiperSlide className="relative w-full h-80 flex justify-center items-center">
-          <div className="h-full w-full absolute  top-0" style={{background:'linear-gradient(0deg, black 0%, rgba(0, 0, 0, 0) 65%)'}}>
+          <div className="h-full w-full absolute  top-0">
           </div>
           <img src="./images/slider-img7.jpg" alt="" className="w-[1250px] h-full object-cover rounded-2xl" />
           <div className='absolute bottom-3 left-20 text-white w-[500px]'>
@@ -39,7 +39,7 @@ export default function Slider() {
           </div>
         </SwiperSlide>
         <SwiperSlide className="w-full h-80 flex justify-center items-center">
-        <div className="h-full w-full absolute  top-0" style={{background:'linear-gradient(0deg, black 0%, rgba(0, 0, 0, 0) 65%)'}}>
+        <div className="h-full w-full absolute  top-0">
         </div>
           <img src="./images/slider-img2.jpg" alt="" className="w-[1250px] h-full object-cover rounded-2xl" />
           <div className='absolute bottom-3 left-20 text-white w-[500px]'>
@@ -52,7 +52,7 @@ export default function Slider() {
           </div>
         </SwiperSlide>
         <SwiperSlide className="w-full h-80 flex justify-center items-center">
-          <div className="h-full w-full absolute  top-0" style={{background:'linear-gradient(0deg, black 0%, rgba(0, 0, 0, 0) 65%)'}}>
+          <div className="h-full w-full absolute  top-0">
           </div>
           <img src="./images/slider-img1.jpg" alt="" className="w-[1250px] h-full object-cover rounded-2xl" />
           <div className='absolute bottom-3 left-20 text-white w-[500px]'>
@@ -65,7 +65,7 @@ export default function Slider() {
           </div>
         </SwiperSlide>
         <SwiperSlide className="w-full h-80 flex justify-center items-center">
-          <div className="h-full w-full absolute  top-0" style={{background:'linear-gradient(0deg, black 0%, rgba(0, 0, 0, 0) 65%)'}}>
+          <div className="h-full w-full absolute  top-0">
           </div>
           <img src="./images/slider-img6.jpg" alt="" className="w-[1250px] h-full object-cover rounded-2xl" />
           <div className='absolute bottom-3 left-20 text-white w-[500px]'>
@@ -78,7 +78,7 @@ export default function Slider() {
           </div>
         </SwiperSlide>
         <SwiperSlide className="w-full h-80 flex justify-center items-center">
-          <div className="h-full w-full absolute  top-0" style={{background:'linear-gradient(0deg, black 0%, rgba(0, 0, 0, 0) 65%)'}}>
+          <div className="h-full w-full absolute  top-0">
           </div>
           <img src="./images/slider-img5.jpg" alt="" className="w-[1250px] h-full object-cover rounded-2xl" />
           <div className='absolute bottom-3 left-20 text-white w-[500px]'>
@@ -91,7 +91,7 @@ export default function Slider() {
           </div>
         </SwiperSlide>
         <SwiperSlide className="w-full h-80 flex justify-center items-center">
-          <div className="h-full w-full absolute  top-0" style={{background:'linear-gradient(0deg, black 0%, rgba(0, 0, 0, 0) 65%)'}}>
+          <div className="h-full w-full absolute  top-0">
           </div>
           <img src="./images/slider-img4.jpg" alt="" className="w-[1250px] h-full object-cover rounded-2xl" />
           <div className='absolute bottom-3 left-20 text-white w-[500px]'>
@@ -104,7 +104,7 @@ export default function Slider() {
           </div>
         </SwiperSlide>
         <SwiperSlide className="w-full h-80 flex justify-center items-center">
-          <div className="h-full w-full absolute  top-0" style={{background:'linear-gradient(0deg, black 0%, rgba(0, 0, 0, 0) 65%)'}}>
+          <div className="h-full w-full absolute  top-0">
           </div>
           <img src="./images/slider-img8.jpg" alt="" className="w-[1250px] h-full object-cover rounded-2xl" />
           <div className='absolute bottom-3 left-20 text-white w-[500px]'>
@@ -117,7 +117,7 @@ export default function Slider() {
           </div>
         </SwiperSlide>
         <SwiperSlide className="w-full h-80 flex justify-center items-center">
-          <div className="h-full w-full absolute  top-0" style={{background:'linear-gradient(0deg, black 0%, rgba(0, 0, 0, 0) 65%)'}}>
+          <div className="h-full w-full absolute  top-0">
           </div>
           <img src="./images/slider-img3.jpg" alt="" className="w-[1250px] h-full object-cover rounded-2xl" />
           <div className='absolute bottom-3 left-20 text-white w-[500px]'>
@@ -130,7 +130,7 @@ export default function Slider() {
           </div>
         </SwiperSlide>
         <SwiperSlide className="w-full h-80 flex justify-center items-center">
-          <div className="h-full w-full absolute  top-0" style={{background:'linear-gradient(0deg, black 0%, rgba(0, 0, 0, 0) 65%)'}}>
+          <div className="h-full w-full absolute  top-0">
           </div>
           <img src="./images/slider-img5.jpg" alt="" className="w-[1250px] h-full object-cover rounded-2xl" />
           <div className='absolute bottom-3 left-20 text-white w-[500px]'>
