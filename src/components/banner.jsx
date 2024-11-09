284b565d01849f3ef617085b5ab01ee0e09d1b21
Md-sarfraz <mdsarfraz62070@gmail.com>
2024-11-09 16:36:26 +0530

    working on about section

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Banner = (props) => {
     return (
         <div>
-            <div className="relative w-full ">
+            <div className="relative w-full bg-[url('/image/bg-banner-img1.png')] bg-cover bg-center">
                 <img src="./images/seat-bg.png" alt="Background" className="w-full h-[70vh] object-cover " />
                 <div className="absolute top-0 left-0 w-full h-[70vh] bg-black opacity-70"></div>
                 <div className="absolute inset-0 flex flex-col items-center justify-center text-center">
@@ -12,6 +12,9 @@ const Banner = (props) => {
                         {props.paragraph}
                     </p>
                 </div>
+            </div>
+            <div className='pt-2'>
+                <img src="./images/image-lines-header.jpg" alt="" />
             </div>
         </div>
     )
--- src/views/about.jsx
+++ src/views/about.jsx
@@ -4,8 +4,31 @@ import Banner from "../components/banner";
 const About = () => {
   return (
     <div>
-    <Banner heading="About Us" paragraph="Welcome to Movie Tickets Booking & Resell - your platform for buying, selling, and transferring movie tickets effortlessly"/>
-    
+      <Banner heading="About Us" paragraph="Welcome to Movie Tickets Booking & Resell - your platform for buying, selling, and transferring movie tickets effortlessly" />
+      <div className="flex flex-row  h-[100vh] px-20">
+        <div className="w-1/2 flex justify-center items-center">
+          <img src="./images/about-Img1.jpg" alt="" className="w-96" />
+        </div>
+        <div className="w-1/2 flex flex-col justify-center gap-1">
+          <img src="./images/film-logo.png" className="w-8" alt="" />
+          <h2 className="text-sm text-stone-400 font-bold">Get To Know</h2>
+          <h1 className="text-3xl font-bold">Providing The Best Ticket Booking Service</h1>
+          <p className="pt-4 text-stone-400 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti quis quasi rerum quo vitae ullam odio reprehenderit. Nam nobis, asperiores sapiente beatae error repudiandae consequuntur deleniti, iure aliquam fugiat necessitatibus.</p>
+          <div className="flex flex-row gap-4 items-center pt-4">
+            <div className="w-14">
+              <img src="./images/about-logo2.png" alt="" />
+            </div>
+            <div>
+              <h1>6 Years of Innovation</h1>
+              <p>We're here for look even you from start to finish.</p>
+            </div>
+          </div>
+          <div className='pt-9'>
+          <button className='bg-red-500 w-40 h-10 text-xs font-bold text-white hover:bg-black hover:text-white transition duration-300 ease-in-out hover:scale-105'>Discover More</button>
+        </div>
+        </div>
+        
+      </div>
     </div>
   );
 };
