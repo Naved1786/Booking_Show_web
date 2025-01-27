7bbd0c82e353bd9487c691d24605120e7683ac0c
Md-sarfraz <mdsarfraz62070@gmail.com>
2025-01-27 16:19:31 +0530

    update button

--- src/views/paymentDetails.jsx
+++ src/views/paymentDetails.jsx
@@ -44,7 +44,7 @@ const PaymentDetails = ({ selectedSeats }) => {
         onClick={handlePayment}
         className="bg-green-500 w-full py-3 mt-6 rounded text-white font-semibold hover:bg-green-600"
       >
-        proceed
+        next
       </button>
     </div>
   );
