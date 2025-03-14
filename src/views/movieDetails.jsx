2d1323e988753e9d9605da4c4a6ad125251e7e5a
Md-sarfraz <mdsarfraz62070@gmail.com>
2025-03-14 23:48:57 +0530

    working on movieDetails page

--- src/views/movieDetails.jsx
+++ src/views/movieDetails.jsx
@@ -46,11 +46,11 @@ const MovieDetails = () => {
             <span className="text-yellow-400 text-xl ml-2">{movie?.rating}%</span>
           </div>
           <div className="flex space-x-4">
-            <button className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-6 rounded"
+            {/* <button className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-6 rounded"
               onClick={() => setTrailerUrl(movie.trailer)}
             >
               Watch Trailer
-            </button>
+            </button> */}
 
             {trailerUrl && (
               <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50">
@@ -73,8 +73,8 @@ const MovieDetails = () => {
               </div>
             )}
             <Link to="/bookTickets">
-              <button className="bg-red-600 hover:bg-red-700 text-white font-semibold py-2 px-6 rounded">
-                Book Tickets
+              <button className="bg-white  hover:bg-red-500 hover:text-white text-sm text-black font-semibold py-2 px-5 rounded-xs">
+                Get Ticket
               </button>
             </Link>
           </div>
