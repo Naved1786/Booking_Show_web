5ad42a76fb1dad2a2f2c2c4630b052cd7131b639
Md-sarfraz <mdsarfraz62070@gmail.com>
2025-05-06 12:46:41 +0000

    Auto-sync from Md-sarfraz/movie-ticket-booking

--- friend-repo/friend-repo/friend-repo/friend-repo/src/App.jsx
+++ friend-repo/friend-repo/friend-repo/friend-repo/src/App.jsx
@@ -28,6 +28,7 @@ import PaymentPage from './views/paymentPage';
 import TicketPage from './views/ticketPage';
 
 function App() {
+  //app.jsx
   const dispatch = useDispatch();
 
   const handleGetUserDetails = async (id) => {
